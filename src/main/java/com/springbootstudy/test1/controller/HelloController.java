e5d392b33070eed67bf4f217a1c7f02d441659d0
lql <a16386294@126.com>
2021-12-16 15:52:04 +0800

    restful

--- src/main/java/com/springbootstudy/test1/controller/HelloController.java
+++ src/main/java/com/springbootstudy/test1/controller/HelloController.java
@@ -1,21 +1,17 @@
 package com.springbootstudy.test1.controller;
 
+import com.springbootstudy.test1.domain.HelloDomain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("hello")
+@RequestMapping("helloController")
 public class HelloController {
-    @GetMapping("printHello")
-    public String printHello(){
-        String s= """
-                {
-                  "name": "li",
-                  "age": 18
-                }""";
 
-        return "hello";
-
+    @GetMapping("person")
+    public HelloDomain person(@RequestParam(value = "name",defaultValue = "world") String name){
+        return new HelloDomain(name+",Ben");
     }
 }
---
+++ src/main/java/com/springbootstudy/test1/domain/HelloDomain.java
@@ -0,0 +1,18 @@
+package com.springbootstudy.test1.domain;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Getter
+public class HelloDomain {
+    private  static int count=0;
+    private final int id;
+    private final String name;
+
+   public HelloDomain(String name){
+        this.id=count++;
+        this.name=name;
+    }
+
+
+}
